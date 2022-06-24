eec375a9f9bc70f0e3bb4c8e1bb64dd8af9347fc
nieeyoung <52353487+nieeyoung@users.noreply.github.com>
2022-06-24 12:16:29 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# intro TO  Java
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nieeyoung/pen/oNEKegZ](https://codepen.io/nieeyoung/pen/oNEKegZ).
+
---
+++ src/script.js
@@ -0,0 +1,6 @@
+const NAME = "Promise Adekoya";
+let height = "\n6'1";
+let country = "\nCanada"
+country = "\nNigeria";
+console.log(NAME, height, country);
+alert(NAME + height + country);
